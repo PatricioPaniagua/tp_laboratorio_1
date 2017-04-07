73798d4a23ddc88bcb9e13a84666d5cb28351e78
PatricioPaniagua <patriciopaniagua95@gmail.com>
2017-04-07 13:31:34 -0300

    asdasd

---
+++ funciones.h
@@ -0,0 +1,28 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+int sumar(int a, int b)
+{
+    int suma;
+    suma = a+b;
+    return suma;
+}
+int restar(int a, int b)
+{
+    int resta;
+    resta = a-b;
+    return resta;
+}
+int dividir(int a, int b)
+{
+    int división;
+    división = a/b;
+    return división;
+}
+int multiplicar(int a, int b)
+{
+    int multiplicación;
+    multiplicación = a*b;
+    return multiplicación;
+}
+#endif // FUNCIONES_H_INCLUDED
